7ba01f6b570a2e554ce6d52802c63eae29a88767
leomuko <kezzieleo@gmail.com>
2020-05-12 10:20:54 +0300

    Add UI

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.simplecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
